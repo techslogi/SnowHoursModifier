4d9ef9c76e1cd8b66273ed3f0e336979cbff91af
Gabriel Vicente Boçon <32103117+techslogi@users.noreply.github.com>
2019-01-10 10:15:21 -0200

    Add files via upload
    Return date format to SNOW default.

---
+++ SNOW Fix hours.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         SNOW Fix hours
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @updateURL    https://github.com/techslogi/SnowHoursModifier/raw/master/SNOW_Hours_Modifier.user.js
+// @downloadURL	 https://github.com/techslogi/SnowHoursModifier/raw/master/SNOW_Hours_Modifier.user.js
+// @description  Reduces hours from a snow incident;
+// @author       Gabriel Vicente
+// @match        *://itsmgbpeu.service-now.com/*
+// @require      https://code.jquery.com/jquery-3.3.1.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.js
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+
+    var currentURL = window.location.href.toString();
+    if(currentURL.includes("incident.do") || currentURL.includes("incident_list.do")){
+        try{
+            var dateTime;
+            var i;
+            var hoursIncident;
+            var hoursList;
+            var hoursToReduce = 3;
+            hoursIncident = document.getElementsByClassName("activity_date");
+            hoursList = document.getElementsByClassName("datex date-calendar");
+            if(hoursIncident[0] != undefined){
+                for(i=0;i<hoursIncident.length;i++){
+                    dateTime = moment(hoursIncident[i].textContent.toString().replace(/ /g, "T")).subtract(hoursToReduce, 'hours').format("YYYY-MM-DD HH:mm:ss");
+                    hoursIncident[i].textContent = dateTime;
+                }
+            }
+            if(hoursList[0] != undefined){
+                for(i=0;i<hoursList.length;i++){
+                    dateTime = moment(hoursList[i].textContent.toString().replace(/ /g, "T")).subtract(hoursToReduce, 'hours').format("YYYY-MM-DD HH:mm:ss");
+                    hoursList[i].textContent = dateTime;
+                }
+            }
+        }catch(e){
+            alert("error: " + e.message);
+        }
+    }
+
+})();
+
